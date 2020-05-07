6c546aacba4a09a0f51c5345389590081a510abf
Rafael Pacheco <rpacheco@cimne.upc.edu>
2020-05-07 20:22:07 +0200

    Updated geometries.

--- decrypted/lab/lab4.m
+++ decrypted/lab/lab4.m
@@ -566,12 +566,355 @@ Drawing(6).component(accComp).name=['Edge_',4];
 Drawing(6).component(accComp).fnc=Drawing(6).function(2).fnc;
 Drawing(6).component(accComp).data=[10,15,-10,-5];
 
+%% Bolus
+Drawing(7).name='Bolus';
+Drawing(7).function(1).name='hole';
+Drawing(7).function(1).fnc=@(data)circle(data(1),data(2),data(3));
+Drawing(7).function(2).name='edge';
+Drawing(7).function(2).fnc=@(data)line(data(1),data(2),data(3),data(4));
+Drawing(7).color.off=color.lightBlue;
+Drawing(7).color.on=color.lightRed;
+Drawing(7).color.fix=color.black;
+Drawing(7).color.joint=color.lightGreen;
+
+Drawing(7).contextMenu = uicontextmenu;
+FixDraw1=uiFix(Drawing(7).contextMenu);
+uiFixAdd(FixDraw1);
+uiUnFixAdd(FixDraw1)
+ConDraw1=uiCon(Drawing(7).contextMenu);
+uiConAdd(ConDraw1);
+uiUnConAdd(ConDraw1)
 
 
+accComp=0;
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-7;
+x0=-16 ;
+Drawing(7).component(accComp).name=['topLHole_',num2str(ihole)];
+Drawing(7).component(accComp).fnc=Drawing(7).function(1).fnc;
+Drawing(7).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-8;
+x0=-12;
+Drawing(7).component(accComp).name=['topRHole_',num2str(ihole)];
+Drawing(7).component(accComp).fnc=Drawing(7).function(1).fnc;
+Drawing(7).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-12;
+x0=-14;
+Drawing(7).component(accComp).name=['innerHole_',num2str(ihole)];
+Drawing(7).component(accComp).fnc=Drawing(7).function(1).fnc;
+Drawing(7).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=0;
+r=0.5;
+y=-13.143990;
+x0=-14;
+Drawing(7).component(accComp).name=['cdgHole_',num2str(ihole)];
+Drawing(7).component(accComp).fnc=Drawing(7).function(1).fnc;
+Drawing(7).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-19;
+x0=-16;
+Drawing(7).component(accComp).name=['bottomLHole_',num2str(ihole)];
+Drawing(7).component(accComp).fnc=Drawing(7).function(1).fnc;
+Drawing(7).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-18;
+x0=-12;
+Drawing(7).component(accComp).name=['bottomRHole_',num2str(ihole)];
+Drawing(7).component(accComp).fnc=Drawing(7).function(1).fnc;
+Drawing(7).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-20;
+x0=-14;
+Drawing(7).component(accComp).name=['bottomCHole_',num2str(ihole)];
+Drawing(7).component(accComp).fnc=Drawing(7).function(1).fnc;
+Drawing(7).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
 
 
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',1];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-15,-13,-5,-5];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',2];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-13,-11,-5,-6];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',3];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-11,-10,-6,-9];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',4];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-10,-11,-9,-11];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',5];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-11,-12,-11,-12];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',6];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-12,-13,-12,-14];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',7];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-13,-10,-14,-16];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',8];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-10,-10,-16,-19];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',9];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-10,-12,-19,-21];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',10];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-12,-16,-21,-21];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',11];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-16,-18,-21,-19];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',12];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-18,-18,-19,-16];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',13];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-18,-15,-16,-14];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',14];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-15,-16,-14,-12];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',15];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-16,-17,-12,-11];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',16];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-17,-18,-11,-9];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',17];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-18,-17,-9,-6];
+
+accComp=accComp+1;
+
+Drawing(7).component(accComp).name=['Edge_',18];
+Drawing(7).component(accComp).fnc=Drawing(7).function(2).fnc;
+Drawing(7).component(accComp).data=[-17,-15,-6,-5];
+
+%% Arrow
+Drawing(8).name='Arrow';
+Drawing(8).function(1).name='hole';
+Drawing(8).function(1).fnc=@(data)circle(data(1),data(2),data(3));
+Drawing(8).function(2).name='edge';
+Drawing(8).function(2).fnc=@(data)line(data(1),data(2),data(3),data(4));
+Drawing(8).color.off=color.lightBlue;
+Drawing(8).color.on=color.lightRed;
+Drawing(8).color.fix=color.black;
+Drawing(8).color.joint=color.lightGreen;
+
+Drawing(8).contextMenu = uicontextmenu;
+FixDraw1=uiFix(Drawing(8).contextMenu);
+uiFixAdd(FixDraw1);
+uiUnFixAdd(FixDraw1)
+ConDraw1=uiCon(Drawing(8).contextMenu);
+uiConAdd(ConDraw1);
+uiUnConAdd(ConDraw1)
 
 
+accComp=0;
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-11;
+x0=26 ;
+Drawing(8).component(accComp).name=['topLHole_',num2str(ihole)];
+Drawing(8).component(accComp).fnc=Drawing(8).function(1).fnc;
+Drawing(8).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-12;
+x0=28;
+Drawing(8).component(accComp).name=['topRHole_',num2str(ihole)];
+Drawing(8).component(accComp).fnc=Drawing(8).function(1).fnc;
+Drawing(8).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-18;
+x0=27;
+Drawing(8).component(accComp).name=['innerHole_',num2str(ihole)];
+Drawing(8).component(accComp).fnc=Drawing(8).function(1).fnc;
+Drawing(8).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=0;
+r=0.5;
+y=-16.170419;
+x0=27;
+Drawing(8).component(accComp).name=['cdgHole_',num2str(ihole)];
+Drawing(8).component(accComp).fnc=Drawing(8).function(1).fnc;
+Drawing(8).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-17;
+x0=24;
+Drawing(8).component(accComp).name=['bottomLHole_',num2str(ihole)];
+Drawing(8).component(accComp).fnc=Drawing(8).function(1).fnc;
+Drawing(8).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-16;
+x0=30;
+Drawing(8).component(accComp).name=['bottomRHole_',num2str(ihole)];
+Drawing(8).component(accComp).fnc=Drawing(8).function(1).fnc;
+Drawing(8).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+for ihole=1:1
+    accComp=accComp+1;
+dl=6;
+r=0.5;
+y=-21;
+x0=27;
+Drawing(8).component(accComp).name=['bottomCHole_',num2str(ihole)];
+Drawing(8).component(accComp).fnc=Drawing(8).function(1).fnc;
+Drawing(8).component(accComp).data=[x0+dl*(ihole-1),y,r];
+end
+
+
+accComp=accComp+1;
+
+Drawing(8).component(accComp).name=['Edge_',1];
+Drawing(8).component(accComp).fnc=Drawing(8).function(2).fnc;
+Drawing(8).component(accComp).data=[25,29,-10,-10];
+
+accComp=accComp+1;
+
+Drawing(8).component(accComp).name=['Edge_',2];
+Drawing(8).component(accComp).fnc=Drawing(8).function(2).fnc;
+Drawing(8).component(accComp).data=[29,29,-10,-15];
+
+accComp=accComp+1;
+
+Drawing(8).component(accComp).name=['Edge_',3];
+Drawing(8).component(accComp).fnc=Drawing(8).function(2).fnc;
+Drawing(8).component(accComp).data=[29,22,-15,-15];
+
+accComp=accComp+1;
+
+Drawing(8).component(accComp).name=['Edge_',4];
+Drawing(8).component(accComp).fnc=Drawing(8).function(2).fnc;
+Drawing(8).component(accComp).data=[33,27,-15,-23];
+
+accComp=accComp+1;
+
+Drawing(8).component(accComp).name=['Edge_',5];
+Drawing(8).component(accComp).fnc=Drawing(8).function(2).fnc;
+Drawing(8).component(accComp).data=[27,21,-23,-15];
+
+accComp=accComp+1;
+
+Drawing(8).component(accComp).name=['Edge_',6];
+Drawing(8).component(accComp).fnc=Drawing(8).function(2).fnc;
+Drawing(8).component(accComp).data=[21,25,-15,-15];
+
+accComp=accComp+1;
+
+Drawing(8).component(accComp).name=['Edge_',7];
+Drawing(8).component(accComp).fnc=Drawing(8).function(2).fnc;
+Drawing(8).component(accComp).data=[25,25,-15,-10];
 
 
 
